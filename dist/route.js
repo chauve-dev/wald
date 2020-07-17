e729af126d4f4f7e45fa80ae1b71363f76da29b6
jeantet <joey.jeantet@gmail.com>
2020-07-17 14:10:40 +0200

    Supression du dist

--- dist/route.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = [
-    { path: "/", controller: "indexController", type: "get" },
-    { path: "/test", controller: "testController", type: "get" }
-];
