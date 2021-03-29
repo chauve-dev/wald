1bd2b7a73043eb5d4e2d3d0c714cebb781d89bbd
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-03-29 18:22:24 +0200

    Correction typo

--- src/controller/middleware/indexController.ts
+++ src/controller/middleware/indexController.ts
@@ -1,6 +1,6 @@
-import controllerMiddelware from "../../app/controllerMiddleware";
+import controllerMiddleware from "../../app/controllerMiddleware";
 
-export default class indexController extends controllerMiddelware{
+export default class indexController extends controllerMiddleware{
     index(){
         console.log('middleware');
         this.next();
